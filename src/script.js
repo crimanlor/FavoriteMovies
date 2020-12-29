c8863add1f0c9a6eff1e25f2d7670b8bba786c1e
Lorena <criado.manzaneque@gmail.com>
2020-12-29 11:50:30 +0100

    patch is working

--- src/script.js
+++ src/script.js
@@ -30,7 +30,7 @@ peliculaForm.addEventListener("submit", (e) => {
   fetch(apiPeliculas, {
     method: "POST",
     headers: {
-      "Content-Type": "aplicación / json",
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       nombre: `${event.target.nombre.value}`,
@@ -38,31 +38,23 @@ peliculaForm.addEventListener("submit", (e) => {
       director: `${event.target.director.value}`,
       clasificacion: `${event.target.clasificacion.value}`,
     }),
-  })
-    .then((response) => response.json())
-    .then((pelicula) => {
-      peliculaCollection.innerHTML += `<div class="card" style="width: 18rem;" id=${pelicula.id}>
-          <img src="${pelicula.imagen}" class="card-img-top" alt="pelicula imagen">
-          <div class="card-body card text-center">
-          <h5 class="card-title">${pelicula.nombre}</h5>
-          <p class="card-text">Director: ${pelicula.director}</p>
-          <h6 class="card-subtitle mb-2 text-muted">${pelicula.clasificacion}</h6>
-          <button id ="edit-${pelicula.id}" data-id="${pelicula.id}" data-action="edit" class="btn btn-outline-danger my-2">Editar</button>
-          <button id="delete-${pelicula.id}" data-id="${pelicula.id}" data-action="delete" class="btn btn-danger">Delete</button>          
-          </div>
-      </div>`;
-    });
+  });
+  // .then((response) => response.json())
+  // .then((pelicula) => {
+  //   peliculaCollection.innerHTML += `<div class="card" style="width: 18rem;" id=${pelicula.id}>
+  //       <img src="${pelicula.imagen}" class="card-img-top" alt="pelicula imagen">
+  //       <div class="card-body card text-center">
+  //       <h5 class="card-title">${pelicula.nombre}</h5>
+  //       <p class="card-text">Director: ${pelicula.director}</p>
+  //       <h6 class="card-subtitle mb-2 text-muted">${pelicula.clasificacion}</h6>
+  //       <button id ="edit-${pelicula.id}" data-id="${pelicula.id}" data-action="edit" class="btn btn-outline-danger my-2">Editar</button>
+  //       <button id="delete-${pelicula.id}" data-id="${pelicula.id}" data-action="delete" class="btn btn-danger">Delete</button>
+  //       </div>
+  //   </div>`;
+  // });
 });
 
 ///EDIT,DELETE///
-// peliculaCollection.addEventListener("click", (e) => {
-//   if (e.target.dataset.action === "edit") {
-//     const peliculaData = allPeliculas.find((pelicula) => {
-//       return pelicula.id == e.target.dataset.id;
-//     });
-//     console.log(peliculaData);
-//   }
-// });
 
 peliculaCollection.addEventListener("click", (e) => {
   if (e.target.dataset.action === "edit") {
@@ -72,15 +64,57 @@ peliculaCollection.addEventListener("click", (e) => {
     const peliculaData = allPeliculas.find((pelicula) => {
       return pelicula.id == e.target.dataset.id;
     });
-    e.target.parentElement.innerHTML += `<div id = 'edit-pelicula'> 
-      <form class="my-2" id =" edit-pelicula-form "> 
-        <input class="my-1" required id ="edit-nombre" placeholder =" ${peliculaData.nombre} "> 
-        <input class="my-1" required id ="edit-imagen" placeholder =" ${peliculaData.imagen} "> 
-        <input class="my-1" required id ="edit-director" placeholder =" ${peliculaData.director} "> 
-        <input class="my-1" required id = "edit-clasificacion" placeholder= "${peliculaData.clasificacion} "> 
+
+    e.target.parentElement.innerHTML += `<div id="edit-film">
+      <form class="my-2" id ="edit-pelicula-form">
+        <input class="my-1" required id ="edit-nombre" placeholder =" ${peliculaData.nombre} ">
+        <input class="my-1" required id ="edit-imagen" placeholder =" ${peliculaData.imagen} ">
+        <input class="my-1" required id ="edit-director" placeholder =" ${peliculaData.director} ">
+        <input class="my-1" required id = "edit-clasificacion" placeholder= "${peliculaData.clasificacion} ">
         <input class="btn btn-dark my-1" type ="submit" value ="Guardar cambios">
+      </form>
     </div> `;
-  } else if (e.target.dataset.action === "eliminar") {
-    console.log("presionaste eliminar");
+
+    const editPeliculaForm = document.querySelector("#edit-pelicula-form");
+
+    editPeliculaForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const nombreInput = editPeliculaForm.querySelector("#edit-nombre").value;
+      const imagenInput = editPeliculaForm.querySelector("#edit-imagen").value;
+      const directorInput = editPeliculaForm.querySelector("#edit-director")
+        .value;
+      const clasificacionInput = editPeliculaForm.querySelector(
+        "#edit-clasificacion"
+      ).value;
+      const editedPelicula = document.getElementById(`${peliculaData.id}`);
+
+      fetch(`${apiPeliculas}/${peliculaData.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          nombre: nombreInput,
+          imagen: imagenInput,
+          director: directorInput,
+          clasificacion: clasificacionInput,
+        }),
+      })
+        .then((response) => response.json())
+        .then((pelicula) => {
+          editedPelicula.innerHTML = `<div class="card" style="width: 18rem;" id=${pelicula.id}>
+              <img src="${pelicula.imagen}" class="card-img-top" alt="pelicula imagen">
+              <div class="card-body card text-center">
+              <h5 class="card-title">${pelicula.nombre}</h5>
+              <p class="card-text">Director: ${pelicula.director}</p>
+              <h6 class="card-subtitle mb-2 text-muted">${pelicula.clasificacion}</h6>
+              <button id ="edit-${pelicula.id}" data-id="${pelicula.id}" data-action="edit" class="btn btn-outline-danger my-2">Editar</button>
+              <button id="delete-${pelicula.id}" data-id="${pelicula.id}" data-action="delete" class="btn btn-danger">Delete</button>
+              </div>
+          </div>`;
+
+          editPeliculaForm.innerHTML = "";
+        });
+    });
   }
-}); // fin de eventListener para editar y eliminar un libro
+});
